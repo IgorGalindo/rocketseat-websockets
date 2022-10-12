d3dd1b051e369988bf89342e9544dccf42500332
Igor Galindo <igorgalindopaiva@gmail.com>
2022-10-12 11:56:27 -0300

    creating conversation start event

--- src/websocket/ChatService.ts
+++ src/websocket/ChatService.ts
@@ -1,8 +1,18 @@
 import { io } from '../http';
+import { container } from 'tsyringe'
+import { CreateUserService } from '../services/CreateUserService';
 
 io.on('connect', socket => {
-    socket.emit('chat_iniciado', {
-        message: 'Seu chat foi iniciado!'
-    })
+    socket.on('start', async (data) => {
+        const { name, email, avatar } = data;
+        const createUserService = container.resolve(CreateUserService);
+
+        const user = await createUserService.execute({
+            email,
+            avatar,
+            name,
+            socket_id: socket.id,
+        });
+    });
 });
 
